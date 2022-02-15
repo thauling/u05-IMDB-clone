62b2903de5b3af044843099bc2b9e7b8246ca8b6
thauling <hauling.thomas@gmail.com>
2022-02-15 12:44:29 +0000

    change Schema::create attributes in create_reviews_table

--- project/app/database/migrations/2022_02_13_124240_create_reviews_table.php
+++ project/app/database/migrations/2022_02_13_124240_create_reviews_table.php
@@ -19,9 +19,11 @@ return new class extends Migration
             $table->tinyInteger('review_rating')->unsigned();
             //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); 
             //$table->foreignId('user_id')->constrained('users'); //ought to work as of L8
+            $table->foreignId('user_id');
+            $table->foreignId('movie_id');
             //$table->foreignId('movie_id')->constrained('movies'); //supposedly this auto-assumes 'id' in movies table 
-            $table->integer('user_id')->unsigned(); // from Sebbe's example 
-            $table->integer('movie_id')->unsigned(); 
+            //$table->integer('user_id')->unsigned(); // from Sebbe's example 
+            //$table->integer('movie_id')->unsigned(); 
             $table->timestamps();
         });
     }
