b71b753c8d947d3a6656cdc7333a83be585da64c
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-02-18 23:52:26 +0100

    Create movie factory, create movie seeder

--- project/app/database/seeders/DatabaseSeeder.php
+++ project/app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use App\Models\Movie;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Movie::factory(10)->create();
+
+        // DB::table('movies')->insert([
+        //     'title' => Str::random(10),
+        //     'genre' => Str::random(7),
+        //     'cast' => [Str::random(10), Str::random(5), Str::random(7)],
+        //     'abstract' => Str::random(20),
+        //     'urls_images' => [Str::random(10), Str::random(5)],
+        //     'url_trailer' => Str::random(30),
+        //     'avg_rating' => rand(0, 10) / 10
+        // ]);
         // \App\Models\User::factory(10)->create(); //10 = 10 rows I guess
         /* or more specifically:
         $user = User::factory()->create([
