3d6ac9cffa93acd765da75c9d493219ca61f26c8
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-02-19 00:01:45 +0100

    Remove excessive db seeder file, add Review factory, update movie factory

--- project/app/database/seeders/DatabaseSeeder.php
+++ project/app/database/seeders/DatabaseSeeder.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\User;
 use App\Models\Movie;
+use App\Models\Review;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,25 +19,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Movie::factory(10)->create();
-
-        // DB::table('movies')->insert([
-        //     'title' => Str::random(10),
-        //     'genre' => Str::random(7),
-        //     'cast' => [Str::random(10), Str::random(5), Str::random(7)],
-        //     'abstract' => Str::random(20),
-        //     'urls_images' => [Str::random(10), Str::random(5)],
-        //     'url_trailer' => Str::random(30),
-        //     'avg_rating' => rand(0, 10) / 10
-        // ]);
-        // \App\Models\User::factory(10)->create(); //10 = 10 rows I guess
-        /* or more specifically:
-        $user = User::factory()->create([
-            'name' => 'John Smith',
-            'email' => 'john@example.com',
-            'password' => bcrypt('password')
-        ]);
-
-        */
+        User::factory(10)->create(); //10 = 10 rows I guess?
+        Movie::factory(100)->create();
+        Review::factory(20)->create();
     }
 }
