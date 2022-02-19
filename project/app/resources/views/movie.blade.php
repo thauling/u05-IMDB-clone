7d0140b05ad7f2d9623365df8a17c5ae7f487156
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-02-19 20:15:37 +0100

    Add title, release date, img and trailer embed to single movie view

--- project/app/resources/views/movie.blade.php
+++ project/app/resources/views/movie.blade.php
@@ -1,3 +1,16 @@
+<?php 
+  $imgsToArray = json_decode($movie->urls_images); 
+  
+  $imgPath = asset('assets/images/' . $imgsToArray[0]);
+?>
+
 <x-layout>
     <h1>{{ $movie->title }}</h1>
+    <p>{{ $movie->released }}</p>
+    <img width="560" src="{{ $imgPath }}" alt="movie poster" />
+    <iframe width="560" height="315" src="{{ $movie->url_trailer }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    @foreach (json_decode($movie->cast) as $actor)
+    <p>{{ $actor }}</p>
+
+    @endforeach
 </x-layout>
