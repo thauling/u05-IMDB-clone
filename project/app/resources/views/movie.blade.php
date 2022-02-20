b4de4c9836e7db2c15816f218a9e5d34805c4ad3
Sandra Wallén <75799823+sandra-wallen@users.noreply.github.com>
2022-02-20 22:05:53 +0100

    Add css file

--- project/app/resources/views/movie.blade.php
+++ project/app/resources/views/movie.blade.php
@@ -7,10 +7,10 @@
 <x-layout>
   <h1>{{ $movie->title }}</h1>
   <p>{{ $movie->released }}</p>
-  <p>{{ $movie->genre }}</p>
   <p>{{ $movie->avg_rating }}</p>
-  <img width="280" src="{{ $imgPath }}" alt="movie poster" />
-  <iframe width="560" height="315" src="{{ $movie->url_trailer }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <img height="505" src="{{ $imgPath }}" alt="movie poster" />
+  <iframe width="853" height="505" src="{{ $movie->url_trailer }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <p>{{ $movie->genre }}</p>
   @foreach (json_decode($movie->cast) as $actor)
     <p>{{ $actor }}</p>
   @endforeach
