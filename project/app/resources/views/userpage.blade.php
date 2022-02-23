512efc0e1060b3dbb0c9aa9f92ecaf2529e54838
Simon Miller <mail@simonmiller.se>
2022-02-23 11:46:48 +0100

    resolve conflicts in DatabaseSeeder and movies_table

---
+++ project/app/resources/views/userpage.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="app.css">
+    <title>Userpage</title>
+</head>
+<body>
+    <h1>New IMDB page</h1>
+        <h2>Example_user</h2>
+    <div class="container">
+        <img src="images/dog.webp" alt="placeholder">
+        <p class="bio">Hello there! I am a big movie fan and love to spend my time browsing the latest movies. My favourite genre is comedy and horror.</p>
+    </div>
+    <div class="container_2">
+        <h2 class="options">Your watchlist</h2>
+        <h2 class="options">Your reviews/ratings</h2>
+    </div>
+    <div class="footer">
+    <footer>
+        <p>Contact us:</p>
+        <br>
+        <ul>
+            <li>Mail: mail@mail.com</li>
+            <li>Phone: 123456789</li>
+            <li>Adress: Adresstreet 54</li>
+        </ul>
+    </footer>
+    </div>
+</body>
+</html>
