2ebcc0d3b3e0ba26a895022cd46a6f7d8ddc27f8
Simon Miller <mail@simonmiller.se>
2022-02-23 11:24:25 +0100

    resolve conflicts in DatabaseSeeder and movies_table

--- project/app/database/seeders/DatabaseSeeder.php
+++ project/app/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Movie;
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -23,66 +21,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        User::factory(10)->create();
-        Review::factory(20)->create();
-        // Movie::factory(100)->create();
-
-        
-
-        /**** SHOULD THESE API REQUESTS BE SOMEWHERE ELSE? IN THE MOVIE FACTORY FILE? ****/
-        // Get popular movies from TMDB
-        $movies = Http::get('https://api.themoviedb.org/3/movie/popular?api_key=87a6bee8df47d296511c8924683d6ecf&language=en-US&page=1');
-        $moviesToArray = json_decode($movies); // Convert to array
-
-        // Genres are defined as a seperate endpoint and are refered to by their ID in the movie object
-        function getGenre ($id) { // Get the genre by the genre ID in the movie object
-            $genres = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key=87a6bee8df47d296511c8924683d6ecf&language=en-US');
-            $genresToArray = json_decode($genres);
-
-            foreach ($genresToArray->genres as $genre) { // Loop through all genres to find the match, return the genre name
-                if ($genre->id == $id) {
-                    return $genre->name;
-                }
-            }
-        }
-
-        // Trailers are defined as a seperate endpoint
-        function getTrailer ($id) { // Get the trailer youtube key with the movie ID
-            $trailer = Http::get("https://api.themoviedb.org/3/movie/$id/videos?api_key=87a6bee8df47d296511c8924683d6ecf&language=en-US");
-            $trailerToArray = json_decode($trailer);
-
-            if (!$trailerToArray->results == []) { // If results is NOT empty array
-                
-                $trailerId = $trailerToArray->results[0]->key;
-
-                return "https://www.youtube.com/embed/$trailerId"; // Return embed url
-            } else {
-                return "";
-            }
-            
-        }
-
-        // Loop through API response
-        foreach ($moviesToArray->results as $movie) {
-
-            // Actors of a specific movie are defined as a seperate endpoint
-            $actors = Http::get("https://api.themoviedb.org/3/movie/$movie->id/credits?api_key=87a6bee8df47d296511c8924683d6ecf&language=en-US");
-            $actorsToArray = json_decode($actors);
-
-            Movie::create([ // Seed movies table with the API responses
-                'title' => $movie->original_title,
-                'genre' => getGenre($movie->genre_ids[0]),
-                'cast' => json_encode(array($actorsToArray->cast[0]->name, $actorsToArray->cast[1]->name, $actorsToArray->cast[2]->name)),
-                'abstract' => $movie->overview,
-                'urls_images' => json_encode(array($movie->poster_path)),
-                'url_trailer' => getTrailer($movie->id),
-                'avg_rating' => $movie->vote_average,
-                'released' => (int)substr($movie->release_date, 0, 4)
-            ]);
-        }
-
-=======
         User::truncate();
         Movie::truncate();
 
@@ -163,6 +101,5 @@ class DatabaseSeeder extends Seeder
             ],
             'avg_rating' => 5.00,
         ]);
->>>>>>> 866b18aac92d5a7168d44ae6c76ebc906084976d
     }
 }
