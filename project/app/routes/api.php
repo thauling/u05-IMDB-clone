04a9bde1dbb2caaa167d7cb8ee70db802a859d25
thauling <hauling.thomas@gmail.com>
2022-03-04 18:08:39 +0000

    refac: cleanup, remove unused code

--- project/app/routes/api.php
+++ project/app/routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\MovieController;
-use App\Http\Controllers\ReviewController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,18 +15,4 @@ use App\Http\Controllers\ReviewController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
-// });
-
-// public routes: anyone is allowed to do this:
-Route::get('/movies', [MovieController::class, 'index']);
-Route::get('/movies/{id}', [MovieController::class, 'show']);
-Route::get('/movies/{name}', [MovieController::class, 'search']); //consider other search params (actors, genre etc)
-// protected routes: only allow authenticated admins to do the following:
-// Route::group(['middleware' => ['auth:sanctum']], function () {
-//     //for movies
-//     Route::post('/movies', [MovieController::class, 'store']);
-//     Route::put('/movies/{id}', [MovieController::class, 'update']);
-//     Route::delete('/movies/{id}', [MovieController::class, 'destroy']);
-//    //for users
-
 // });
