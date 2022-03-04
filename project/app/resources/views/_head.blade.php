9d9afa53809c07486fda6e90c90ce91518e0a477
Simon Miller <mail@simonmiller.se>
2022-03-04 12:40:41 +0100

    fix landing for new tailwind cdn

--- project/app/resources/views/_head.blade.php
+++ project/app/resources/views/_head.blade.php
@@ -5,6 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
